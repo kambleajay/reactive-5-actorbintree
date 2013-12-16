5a91be71df2c0609885ac059c57ba9bed6951393
Ajay <ajay.rivendell@gmail.com>
2013-12-16 22:57:31 +0530

    working to fix gc

--- src/main/scala/actorbintree/BinaryTreeSet.scala
+++ src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -4,9 +4,14 @@
 package actorbintree
 
 import akka.actor._
-import scala.collection.immutable.Queue
-import actorbintree.BinaryTreeSet.{OperationFinished, Contains, ContainsResult, Insert}
+import scala.collection.mutable
+import actorbintree.BinaryTreeSet._
+import actorbintree.BinaryTreeSet.Contains
+import actorbintree.BinaryTreeSet.OperationFinished
+import actorbintree.BinaryTreeSet.ContainsResult
 import scala.Some
+import actorbintree.BinaryTreeSet.Insert
+import actorbintree.BinaryTreeNode.{CopyTo, CopyFinished}
 
 object BinaryTreeSet {
 
@@ -27,18 +32,21 @@ object BinaryTreeSet {
     * is completed.
     */
   case class Insert(requester: ActorRef, id: Int, elem: Int) extends Operation
+  case class InsertPending(requester: ActorRef, id: Int, elem: Int) extends Operation
 
   /** Request with identifier `id` to check whether an element `elem` is present
     * in the tree. The actor at reference `requester` should be notified when
     * this operation is completed.
     */
   case class Contains(requester: ActorRef, id: Int, elem: Int) extends Operation
+  case class ContainsPending(requester: ActorRef, id: Int, elem: Int) extends Operation
 
   /** Request with identifier `id` to remove the element `elem` from the tree.
     * The actor at reference `requester` should be notified when this operation
     * is completed.
     */
   case class Remove(requester: ActorRef, id: Int, elem: Int) extends Operation
+  case class RemovePending(requester: ActorRef, id: Int, elem: Int) extends Operation
 
   /** Request to perform garbage collection */
   case object GC
@@ -60,10 +68,10 @@ class BinaryTreeSet extends Actor with ActorLogging {
 
   def createRoot: ActorRef = context.actorOf(BinaryTreeNode.props(0, initiallyRemoved = true))
 
-  var root: Option[ActorRef] = None
+  var root = createRoot
 
   // optional
-  var pendingQueue = Queue.empty[Operation]
+  var pendingQueue: mutable.Queue[Operation] = mutable.Queue.empty[Operation]
 
   // optional
   def receive = normal
@@ -71,26 +79,26 @@ class BinaryTreeSet extends Actor with ActorLogging {
   // optional
   /** Accepts `Operation` and `GC` messages. */
   val normal: Receive = {
-
-    case Insert(caller, id, elem) => {
-      root match {
-        case Some(rnode) => rnode ! Insert(caller, id, elem)
-        case None => {
-          root = Some(context.actorOf(BinaryTreeNode.props(elem, false)))
-          caller ! OperationFinished(id)
-        }
-      }
+    case Insert(caller, id, elem) => root ! Insert(caller, id, elem)
+    case Contains(caller, id, elem) => root ! Contains(caller, id, elem)
+    case Remove(caller, id, elem) => root ! Remove(caller, id, elem)
+    case GC => {
+      val newRoot = createRoot
+      root ! CopyTo(newRoot)
+      context.become(garbageCollecting(newRoot))
     }
+  }
 
-    case Contains(caller, id, elem) => {
-      root match {
-        case Some(rnode) => rnode ! Contains(caller, id, elem)
-        case None => {
-          caller ! ContainsResult(id, false)
-        }
-      }
-    }
+  def processPending: Receive = {
+    case InsertPending(caller, id, elem) => root ! Insert(caller, id, elem)
+    case ContainsPending(caller, id, elem) => root ! Contains(caller, id, elem)
+    case RemovePending(caller, id, elem) => root ! Remove(caller, id, elem)
 
+    case Insert(caller, id, elem) => pendingQueue.enqueue(InsertPending(caller, id, elem))
+    case Contains(caller, id, elem) => pendingQueue.enqueue(ContainsPending(caller, id, elem))
+    case Remove(caller, id, elem) => pendingQueue.enqueue(RemovePending(caller, id, elem))
+
+    case GC => ()
   }
 
   // optional
@@ -98,7 +106,24 @@ class BinaryTreeSet extends Actor with ActorLogging {
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case Insert(caller, id, elem) => pendingQueue.enqueue(InsertPending(caller, id, elem))
+    case Contains(caller, id, elem) => pendingQueue.enqueue(ContainsPending(caller, id, elem))
+    case Remove(caller, id, elem) => pendingQueue.enqueue(RemovePending(caller, id, elem))
+    case CopyFinished => {
+      root ! PoisonPill
+      root = newRoot
+
+      context.become(processPending)
+
+      while (!pendingQueue.isEmpty) {
+        self ! pendingQueue.dequeue
+      }
+
+      context.become(normal)
+    }
+    case GC => ()
+  }
 
 }
 
@@ -114,17 +139,21 @@ object BinaryTreeNode {
 
   case object CopyFinished
 
+  case object PoisonPill
+
   def props(elem: Int, initiallyRemoved: Boolean) = Props(classOf[BinaryTreeNode], elem, initiallyRemoved)
 }
 
 class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor with ActorLogging {
-
 
   //var subtrees = Map[Position, ActorRef]()
   var removed = initiallyRemoved
 
   var left: Option[ActorRef] = None
   var right: Option[ActorRef] = None
+
+  var pendingCopy = 0
+  var selfInsertDone = false
 
   // optional
   def receive = normal
@@ -144,7 +173,7 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor wit
     }
 
     case Contains(caller, id, elemToMatch) => {
-      if (elemToMatch == elem) {
+      if (elemToMatch == elem && !removed) {
         caller ! ContainsResult(id, true)
       } else if (elemToMatch < elem) {
         containsInLeft(caller, id, elemToMatch)
@@ -153,23 +182,54 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor wit
       }
     }
 
+    case Remove(caller, id, elemToRemove) => {
+      if (elemToRemove == elem) {
+        removed = true
+        caller ! OperationFinished(id)
+      } else if (elemToRemove < elem) {
+        removeFromLeft(caller, id, elemToRemove)
+      } else {
+        removeFromRight(caller, id, elemToRemove)
+      }
+    }
+
+    case CopyTo(newRoot) => {
+      val expected = Seq(left, right).filter(_.isDefined).map(_.get).toSet
+      pendingCopy = expected.size
+      var insertDone = false
+      if (!removed) {
+        newRoot ! Insert(self, System.currentTimeMillis.toInt, elem)
+      } else {
+        insertDone = true
+      }
+      if (left.isDefined) left.get ! CopyTo(newRoot)
+      if (right.isDefined) right.get ! CopyTo(newRoot)
+      context.become(copying(sender, expected, insertDone))
+    }
+
+    case PoisonPill => {
+      if (left.isDefined) left.get ! PoisonPill
+      if (right.isDefined) right.get ! PoisonPill
+      context.stop(self)
+    }
+
   }
 
-  def insertInLeft(caller: ActorRef, id: Int, elem: Int) {
+  def insertInLeft(caller: ActorRef, id: Int, newElem: Int) {
     left match {
-      case Some(lnode) => lnode ! Insert(caller, id, elem)
+      case Some(lnode) => lnode ! Insert(caller, id, newElem)
       case None => {
-        left = Some(context.actorOf(BinaryTreeNode.props(elem, false)))
+        left = Some(context.actorOf(BinaryTreeNode.props(newElem, false)))
         caller ! OperationFinished(id)
       }
     }
   }
 
-  def insertInRight(caller: ActorRef, id: Int, elem: Int) {
+  def insertInRight(caller: ActorRef, id: Int, newElem: Int) {
     right match {
-      case Some(rnode) => rnode ! Insert(caller, id, elem)
+      case Some(rnode) => rnode ! Insert(caller, id, newElem)
       case None => {
-        right = Some(context.actorOf(BinaryTreeNode.props(elem, false)))
+        right = Some(context.actorOf(BinaryTreeNode.props(newElem, false)))
         caller ! OperationFinished(id)
       }
     }
@@ -177,7 +237,7 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor wit
 
   def containsInLeft(caller: ActorRef, id: Int, elemToMatch: Int) {
     left match {
-      case Some(lnode) => lnode ! Contains(caller, id, elem)
+      case Some(lnode) => lnode ! Contains(caller, id, elemToMatch)
       case None => {
         caller ! ContainsResult(id, false)
       }
@@ -186,9 +246,27 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor wit
 
   def containsInRight(caller: ActorRef, id: Int, elemToMatch: Int) {
     right match {
-      case Some(rnode) => rnode ! Contains(caller, id, elem)
+      case Some(rnode) => rnode ! Contains(caller, id, elemToMatch)
       case None => {
         caller ! ContainsResult(id, false)
+      }
+    }
+  }
+
+  def removeFromLeft(caller: ActorRef, id: Int, elemToRemove: Int) {
+    left match {
+      case Some(lnode) => lnode ! Remove(caller, id, elemToRemove)
+      case None => {
+        caller ! OperationFinished(id)
+      }
+    }
+  }
+
+  def removeFromRight(caller: ActorRef, id: Int, elemToRemove: Int) {
+    right match {
+      case Some(rnode) => rnode ! Remove(caller, id, elemToRemove)
+      case None => {
+        caller ! OperationFinished(id)
       }
     }
   }
@@ -197,6 +275,19 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor wit
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  def copying(caller: ActorRef, expected: Set[ActorRef], insertConfirmed: Boolean): Receive = {
+    case CopyFinished => {
+      pendingCopy = pendingCopy - 1
+      if (pendingCopy == 0 && selfInsertDone) {
+        caller ! CopyFinished
+      }
+    }
+    case OperationFinished => {
+      selfInsertDone = true
+      if (pendingCopy == 0 && selfInsertDone) {
+        caller ! CopyFinished
+      }
+    }
+  }
 
 }
--- src/test/scala/actorbintree/BinaryTreeSpec.scala
+++ src/test/scala/actorbintree/BinaryTreeSpec.scala
@@ -8,13 +8,88 @@ import scala.concurrent.duration._
 class BinaryTreeSpec extends Specification with NoTimeConversions {
   sequential
 
-//  "Binary tree" should {
-//    "insert given element" in new AkkaTestkitSpecs2Support {
-//       within(1 second) {
-//         val binTree = system.actorOf(Props[BinaryTreeSet])
-//         binTree ! BinaryTreeSet.Insert(self, 1, 10)
-//         expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(1)
-//       }
-//    }
-//  }
+  "Binary tree" should {
+
+    "insert given element" in new AkkaTestkitSpecs2Support {
+      within(1 second) {
+        val binTree = system.actorOf(Props[BinaryTreeSet])
+        binTree ! BinaryTreeSet.Insert(self, 1, 10)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(1)
+
+        binTree ! BinaryTreeSet.Insert(self, 2, 10)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(2)
+      }
+    }
+
+    "give correct answer for contains test" in new AkkaTestkitSpecs2Support {
+      within(1 second) {
+        val binTree = system.actorOf(Props[BinaryTreeSet])
+        binTree ! BinaryTreeSet.Insert(self, 1, 20)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(1)
+
+        binTree ! BinaryTreeSet.Contains(self, 3, 21)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(3, false)
+
+        binTree ! BinaryTreeSet.Contains(self, 5, 20)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(5, true)
+      }
+    }
+
+    "remove given element" in new AkkaTestkitSpecs2Support {
+      within(1 second) {
+        val binTree = system.actorOf(Props[BinaryTreeSet])
+        binTree ! BinaryTreeSet.Insert(self, 1, 30)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(1)
+
+        binTree ! BinaryTreeSet.Remove(self, 2, 31)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(2)
+
+        binTree ! BinaryTreeSet.Contains(self, 3, 30)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(3, true)
+
+        binTree ! BinaryTreeSet.Remove(self, 5, 30)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(5)
+
+        binTree ! BinaryTreeSet.Contains(self, 8, 30)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(8, false)
+      }
+    }
+
+    "properly handle GC" in new AkkaTestkitSpecs2Support {
+      within(1 second) {
+        val binTree = system.actorOf(Props[BinaryTreeSet])
+        binTree ! BinaryTreeSet.Insert(self, 1, 30)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(1)
+
+        binTree ! BinaryTreeSet.Insert(self, 2, 40)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(2)
+
+        binTree ! BinaryTreeSet.Insert(self, 2, 50)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(2)
+
+        binTree ! BinaryTreeSet.Contains(self, 3, 30)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(3, true)
+
+        binTree ! BinaryTreeSet.Remove(self, 5, 30)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(5)
+
+        binTree ! BinaryTreeSet.Contains(self, 8, 30)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(8, false)
+
+        binTree ! BinaryTreeSet.GC
+
+        binTree ! BinaryTreeSet.Contains(self, 10, 30)
+        binTree ! BinaryTreeSet.Insert(self, 11, 77)
+        binTree ! BinaryTreeSet.Remove(self, 12, 77)
+        binTree ! BinaryTreeSet.Contains(self, 13, 77)
+
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(10, false)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(11)
+        expectMsgType[BinaryTreeSet.OperationFinished] must be equalTo BinaryTreeSet.OperationFinished(12)
+        expectMsgType[BinaryTreeSet.ContainsResult] must be equalTo BinaryTreeSet.ContainsResult(13, false)        
+      }
+    }
+
+  }
+
 }
